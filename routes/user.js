8d9a5e6deb3815982779b8c52ee6fe514e409a30
Olivér <olika96@gmail.com>
2017-05-19 23:00:39 +0200

    Oliver javított
    A belépés, a regisztráció javítása

--- routes/user.js
+++ routes/user.js
@@ -12,17 +12,40 @@ router.get('/register', function(req, res, next) {
   /*var newUser = new User();
   res.render('register', {username: '', password: '', email: '', szobaszam: ''});*/
 });
+router.get('/main', function(req, res, next) {
+  res.render('main');
+});
 
 /* SAVE new user*/
 router.post('/new', function(req, res, next){
   var newUser = new User({ username: req.body.username, password: req.body.password, email: req.body.email, szobaszam: req.body.szobaszam });
+	//if(req.body.password1!=req.body.password2)
+		//return res.render('register', { user: newUser, hiba:"jelszo"});
+  
   newUser.save(function(err, doc){
-    if(err) return res.render('register', { user: newUser});
+    if(err){
+      console.log(err);
+     return res.render('register', { user: newUser});
+     } 
     else return res.redirect('/');
   });
-  /*
-    if(err) return res.render('register', { username: req.body.username, password: req.body.password, email: req.body.email, szobaszam: req.body.szobaszam });
-  */
 });
+
+router.post('/login', function(req, res, next){
+  var nUser = new User();
+
+  User.findOne({'username':req.body.username,'password':req.body.password}, function(err, user){
+    if(err){
+      console.log(err);
+    }
+    if(user==null)
+    {
+      return res.redirect('/');
+    }
+    else{
+      return res.redirect('/users/main');
+    }
+  });
+})
 
 module.exports = router;
